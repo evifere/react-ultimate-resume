5ffa08b127f207e4fb07fb897b344c7ebf76fca2
JOLLY <evifere@gmail.com>
2020-04-20 09:59:20 +0200

    set defaut language to fr

--- src/package/components/profile.jsx
+++ src/package/components/profile.jsx
@@ -38,7 +38,7 @@ const messages = {
 const useStyles = createUseStyles(styles);
 
 const DEFAULT_OPTIONS = Object.freeze({
-    locale: 'en',
+    locale: 'fr',
     customization: {
         imageHeader: {
             url: 'https://cdn.filestackcontent.com/8I2wVnCRTFxypXRYLRsp',
